e9aafe5ab04fb92d1e1faf5111e2cc50db4ff6b7
lchansky <lchansky@mail.ru>
2022-04-10 23:58:23 +0300

    In main.py: one more improvement to method change_currency(), added function wallets_rewrite() to Wallet.inc_balance() and Wallet.dec_balance() and other mini-changes

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ class Wallet:
         увеличение и уменьшение баланса кошелька (за счёт расходов/доходов)
         открытие существующих кошельков из файла CSV
     Свойства кошелька:
-        currency - валюта. Поддерживается только 3 валюты - ₽, $, €.
         name - название,
-        balance - баланс.
+        currency - валюта. Поддерживается только 3 валюты - '₽', '$', '€'.
+        balance - баланс float.
     """
 
     def __init__(self, name='Счёт по умолчанию', currency='₽', balance=0.00):
@@ -22,7 +22,7 @@ class Wallet:
         self.currency = currency
         self.__balance = float(balance)
 
-    def __del__(self):
+    def delete(self):
         print(f'Кошелёк "{self.name}" удалён')
         return self.__balance
 
@@ -30,12 +30,13 @@ class Wallet:
         """Возвращает инфу о счёте в виде кортежа (name, currency, balance)"""
         return self.name, self.currency, self.__balance
 
-    def inc_balance(self, num: float):
+    def inc_balance(self, id: int, num: float):
         """Принимает int или float. Увеличивает баланс кошелька"""
         self.__balance += float(num)
+        wallets_rewrite(id, self.name, self.currency, self.__balance)
         return num
 
-    def dec_balance(self, num: float):
+    def dec_balance(self, id: int, num: float):
         """Принимает int или float. Уменьшает баланс кошелька"""
         if self.__balance - num < 0:
             print(
@@ -44,6 +45,7 @@ class Wallet:
             raise ValueError('Недостаточно средств')
         else:
             self.__balance -= num
+        wallets_rewrite(id, self.name, self.currency, self.__balance)
         return num
 
     def add_to_db(self):
@@ -59,37 +61,48 @@ class Wallet:
 
     def change_currency(self, id: int, new_currency: str):
         """Меняет валюту кошелька и переводит деньги по курсу. Вызывает wallets_rewrite() для замены записи в CSV файле
-        Принимает строку - новая валюта, возвращает новый баланс."""
+        Принимает id кошелька и новая валюта (str), возвращает новый баланс."""
 
         if new_currency == self.currency:  # Если новая валюта = старая валюта, то просто возвращает баланс
             return self.__balance
 
-        exchange = currencies[f'{self.currency} {new_currency}']  # Ищет в словаре валют нужную пару
-        self.__balance /= exchange
+        try:
+            exchange = currencies[f'{self.currency} {new_currency}']  # Ищет в словаре валют нужную пару
+            self.__balance /= exchange
+        except KeyError:
+            exchange = currencies[f'{new_currency} {self.currency}']
+            self.__balance *= exchange
+
+        """
+        ЕСЛИ НЕ НАШЁЛ ПАРУ 'СТАРАЯ_ВАЛЮТА НОВАЯ_ВАЛЮТА', 
+        ТО ИСКАТЬ НАОБОРОТ, И НЕ ДЕЛИТЬ А УМНОЖАТЬ
+        """
 
         self.currency = new_currency
-
-        "РЕАЛИЗОВАТЬ В ЭТОЙ ФУНКЦИЮ ЗАМЕНЫ ВАЛЮТЫ В CSV ФАЙЛЕ"
-
         wallets_rewrite(id, self.name, self.currency, self.__balance)
 
         return self.__balance
 
+    def change_name(self, id: int, new_name: str):
+        """Меняет название кошелька. Вызывает wallets_rewrite() для замены записи в CSV файле
+        Принимает id кошелька и новое название, возвращает новый баланс."""
+        if new_name == self.name:  # Если новое название = старое, то ничего не происходит
+            pass
 
-""" Начало работы программы. Считывание всех данных из CSV файлов. """
+        self.name = new_name
+        wallets_rewrite(id, self.name, self.currency, self.__balance)
 
 
-# Загрузка кошельков
 def wallets_reload():
     """Загружает данные о счетах из CSV файла. Возвращает словарь {id : Объект класса Wallet (т.е. счёт)}"""
     w = {}  # wallet
-    print('Обновление данных о счетах...==========')
+    print('\n====== Обновление данных о счетах... ==========')
     with open('wallets.csv', encoding='UTF-8') as file:
         for i in file.readlines():
             par = i.strip().split(',')
             w[int(par[0])] = Wallet(*tuple(par[1:]))
             print(f'Счёт id: {par[0]} загружен: {w[int(par[0])].info()}')
-    print('Данные о кошельках обновлены!==========')
+    print('====== Данные о кошельках обновлены! ==========\n')
     return w
 
 
@@ -118,12 +131,12 @@ def wallets_rewrite(id: int, name: str, currency: str, balance: float):
 def currencies_reload():
     """Загружает курсы валют из CSV файла. Возвращает словарь {currency1-currency2 (str): exchange rate (float), ...}"""
 
-    print('Обновление курсов валют...')
+    print('\n====== Обновление курсов валют... ======')
     exchange_rates = {}
     with open('currencies.csv', encoding='UTF-8') as file:
         for i in file.readlines():
             exchange_rates[i.strip().split(',')[0]] = float(i.strip().split(',')[1])
-    print('Курсы валют обновлены!')
+    print('====== Курсы валют обновлены! ======\n')
     return exchange_rates
 
 
@@ -131,6 +144,15 @@ def currencies_reload():
 wallets = wallets_reload()
 currencies = currencies_reload()
 
-print(wallets[2].info())
-print(wallets[2].change_currency(2, '₽'))
-print(wallets[2].info())
+for i in wallets.keys():
+    print(i, wallets[i].info())
+
+wallets[7].change_currency(7, '₽')
+
+for i in wallets.keys():
+    print(i, wallets[i].info())
+
+print('\n\n====== Конец программы ======\n\n\n')
+
+
+

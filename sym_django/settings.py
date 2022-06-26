9800d1502b1b6a67b2a2ad0165f213a00a67e09c
lchansky <lchansky@mail.ru>
2022-06-26 11:40:10 +0300

    Some mini-improvements for easier using Docker. Now new secret-key generates all times when start server. Added new lib to requirements.txt and changed name at this file (it was typo there). Moved old files of console version to individual folder

--- sym_django/settings.py
+++ sym_django/settings.py
@@ -9,31 +9,23 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import json
 from pathlib import Path
 import os
-# import environ
+from djecrety.utils import generate_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY =
+SECRET_KEY = generate_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = False
 
-with open('sym_django/django_settings.json', 'r') as file:
-    settings = json.loads(file.read())
-    DEBUG = settings['DEBUG']
-    SECRET_KEY = settings['SECRET_KEY']
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -47,6 +39,7 @@ INSTALLED_APPS = [
     
     'sym_app.apps.SymAppConfig',
     
+    'djecrety',
     'debug_toolbar',
 ]
 

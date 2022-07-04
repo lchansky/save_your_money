8cb0c8517c02ec47cae27fbb4102b922b26256b3
lchansky <lchansky@mail.ru>
2022-07-04 18:51:28 +0300

    Moved all files to docker-building structure. Fixed bug in new operation - ZeroDivisionError (I make new validation)

--- sym_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SymAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sym_app'
-    verbose_name = 'Приложение Save Your Money'
